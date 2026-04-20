466602e5022287c56bc9510bf50257dccb09f5a9
mokhtar <moktarmoha17@gmail.com>
2026-04-20 19:27:22 +0200

    admin managment done

--- php-cafetria/db.php
+++ php-cafetria/db.php
@@ -6,9 +6,9 @@ define('DB_NAME', 'cafetria');
 
 try {
     $pdo = new PDO(
-        'mysql:host=' . localhost . ';dbname=' . cafetria . ';charset=utf8mb4',
-        "root",
-        "",
+        'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8mb4',
+        DB_USER,
+        DB_PASS,
         [
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
